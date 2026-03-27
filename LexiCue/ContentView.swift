0eec0560548312fe35afe8f5ae20af9fded60ff7
Yevhen Miliukov <yevhenmiliukov@outlook.com>
2026-03-27 12:19:57 +0200

    Initial Commit

---
+++ LexiCue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LexiCue
+//
+//  Created by Евгений Милюков on 27.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LexiCue/LexiCueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiCueApp.swift
+//  LexiCue
+//
+//  Created by Евгений Милюков on 27.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LexiCueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
